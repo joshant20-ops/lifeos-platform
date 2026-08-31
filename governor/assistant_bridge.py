69f4060d41a4c3a7d094d4ed742c8e53ba5087e9
joshant20-ops <joshant20@gmail.com>
2026-08-31 19:15:04 +0100

    governor: allow Home Assistant to frame assistant UI

--- governor/assistant_bridge.py
+++ governor/assistant_bridge.py
@@ -83,7 +83,6 @@ class Handler(BaseHTTPRequestHandler):
         self.send_header("Content-Type", content_type)
         self.send_header("Content-Length", str(len(data)))
         self.send_header("Cache-Control", "no-store")
-        self.send_header("X-Frame-Options", "SAMEORIGIN")
         self.end_headers()
         self.wfile.write(data)
 
